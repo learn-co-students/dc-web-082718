ed315afc886ddffe526a7734087db1d16d456d98
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-08-30 12:24:26 -0400

    updated code

--- 04-one-to-many/run.rb
+++ 04-one-to-many/run.rb
@@ -4,18 +4,22 @@ require_relative "./user.rb"
 coffee_dad = User.new("Coffee Dad")
 tea_uncle = User.new("Tea Uncle")
 
-# coffee_dad.post_tweet("I need coffee")
-# coffee_dad.post_tweet("I want coffee")
-# coffee_dad.post_tweet("I am drinking coffee")
-# coffee_dad.post_tweet("Where is coffee?")
-# coffee_dad.post_tweet("Life is a meaningless void")
-# tea_uncle.post_tweet("I like tea")
-# tea_uncle.post_tweet("Life is a bunch of flowers")
-# tweet1 = Tweet.all[0]
+coffee_dad.post_tweet("I need coffee")
+coffee_dad.post_tweet("I want coffee")
+coffee_dad.post_tweet("I am drinking coffee")
+coffee_dad.post_tweet("Where is coffee?")
+coffee_dad.post_tweet("Life is a meaningless void")
+tea_uncle.post_tweet("I like tea")
+tea_uncle.post_tweet("Life is a bunch of flowers")
+tweet1 = Tweet.all[0]
 
 # t coffee_dad.tweets 
 # return all coffee dad tweets
 
-# print tweet1.username
+print tweet1.username
 
 print Tweet.all
+
+Tweet.new
+
+tweet1.correct_spelling
