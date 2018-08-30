c43fe7fe540575087e80756c61e18fb77837620f
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-08-30 12:24:03 -0400

    post lecture

--- 04-one-to-many/README.md
+++ 04-one-to-many/README.md
@@ -12,9 +12,10 @@
 ## Deliverables
 
 * Create a User class. The class should have these methods:
-  * `#initialize` which takes a username and have a reader method for the username
+  * `#initialize` which takes a username and has a reader method for the username
   * `#tweets` that returns an array of Tweet instances
   * `#post_tweet` that takes a message, creates a new tweet, and adds it to the user's tweet collection
+
 * Create a Tweet class. The class should have these methods:
   * `Tweet#message` that returns a string
   * `Tweet#user` that returns an instance of the user class
---
+++ 04-one-to-many/run.rb
@@ -0,0 +1,21 @@
+require_relative "./tweet.rb"
+require_relative "./user.rb"
+
+coffee_dad = User.new("Coffee Dad")
+tea_uncle = User.new("Tea Uncle")
+
+# coffee_dad.post_tweet("I need coffee")
+# coffee_dad.post_tweet("I want coffee")
+# coffee_dad.post_tweet("I am drinking coffee")
+# coffee_dad.post_tweet("Where is coffee?")
+# coffee_dad.post_tweet("Life is a meaningless void")
+# tea_uncle.post_tweet("I like tea")
+# tea_uncle.post_tweet("Life is a bunch of flowers")
+# tweet1 = Tweet.all[0]
+
+# t coffee_dad.tweets 
+# return all coffee dad tweets
+
+# print tweet1.username
+
+print Tweet.all
