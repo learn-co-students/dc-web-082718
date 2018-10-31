819719b2ed28a37e44fc315d8d203d43f473ba91
Bruno Garcia Gonzalez <brunogarciagonzalez@outlook.com>
2018-10-31 15:50:34 -0400

    added item 39

---
+++ 39-animal-shelter-review/src/components/Filters.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class Filters extends React.Component {
+  render() {
+    
+    return (
+      <div className="ui form">
+        <h3>Animal type</h3>
+        <div className="field">
+          <select name="type" id="type" onChange={this.props.onChangeType}>
+            <option value="all">All</option>
+            <option value="cat">Cats</option>
+            <option value="dog">Dogs</option>
+            <option value="micropig">Micropigs</option>
+          </select>
+        </div>
+
+        <div className="field">
+          <button className="ui secondary button" onClick={this.props.onFindPetsClick}>Find pets</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Filters
