1af53f21778672547b3c4948c600f9d4f07b9efb
github username <sam.catherman@gmail.com>
2018-11-02 13:53:50 -0400

    delete duplicate

--- 41-hogwarts-review/src/components/HogDetail.js
+++ 41-hogwarts-review/src/components/HogDetail.js
@@ -15,11 +15,13 @@ class HogDetail extends Component {
     const weight =
       "weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water";
     return (
-      <div className="pigTile">
-        <h1 className="headerText">{hog.name}</h1>
-        <img src={this.getHogImgs(hog)} />
-        <p>{hog[weight]} units of hog weight</p>
-      </div>
+      <React.Fragment>
+        <div className="pigTile">
+          <h1 className="headerText">{hog.name}</h1>
+          <img src={this.getHogImgs(hog)} alt={hog.name} />
+          <p>{hog[weight]} units of hog weight</p>
+        </div>
+      </React.Fragment>
     );
   }
 }
